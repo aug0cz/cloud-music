ed2bdf2f91b77266b2cee16146ad36436d55e9ef
aug0cz <aug0cz@163.com>
2021-09-21 10:08:56 +0800

    feat(chapter): chapter-3 轮播图和列表

--- src/application/Recommend/index.tsx
+++ src/application/Recommend/index.tsx
@@ -1,5 +1,33 @@
 import React, { memo } from "react";
+import Slider from "../../components/slider";
+import RecommendList from "../../components/list";
 
-const Recommend: React.FC = () => <div>Recommend</div>;
+const Recommend: React.FC = () => {
+  const bannerList = [1, 2, 3, 4].map((item) => {
+    return {
+      imageUrl:
+        "http://p1.music.126.net/ZYLJ2oZn74yUz5x8NBGkVA==/109951164331219056.jpg",
+    };
+  });
+
+  const recommendList = Array(10)
+    .fill(0)
+    .map((item, idx) => {
+      return {
+        id: idx,
+        picUrl:
+          "https://p1.music.126.net/fhmefjUfMD-8qtj3JKeHbA==/18999560928537533.jpg",
+        playCount: 17171122,
+        name: "朴树、许巍、李健、郑钧、老狼、赵雷",
+      };
+    });
+
+  return (
+    <div>
+      <Slider bannerList={bannerList}></Slider>
+      <RecommendList recommendList={recommendList}></RecommendList>
+    </div>
+  );
+};
 
 export default memo(Recommend);
---
+++ src/components/list/index.tsx
@@ -0,0 +1,47 @@
+import React, { memo } from "react";
+import { getCount } from "../../api/utils";
+import { ListWrapper, ListItem, List } from "./style";
+
+interface IRecommend {
+  id: number;
+  picUrl: string;
+  playCount: number;
+  name: string;
+}
+
+type RecommendListProps = {
+  recommendList: IRecommend[];
+};
+
+const RecommendList: React.FC<RecommendListProps> = (props) => {
+  const { recommendList } = props;
+  return (
+    <ListWrapper>
+      <h1 className="title">推荐歌单</h1>
+      <List>
+        {recommendList.map((item, idx) => {
+          return (
+            <ListItem key={item.id}>
+              <div className="img_wrapper">
+                <div className="decorate"></div>
+                <img
+                  src={item.picUrl + "?param=300x300"}
+                  width="100%"
+                  height="100%"
+                  alt="music"
+                />
+                <div className="play_count">
+                  <i className="iconfont play">&#xe885;</i>
+                  <span className="count">{getCount(item.playCount)}</span>
+                </div>
+              </div>
+              <div className="desc">{item.name}</div>
+            </ListItem>
+          );
+        })}
+      </List>
+    </ListWrapper>
+  );
+};
+
+export default memo(RecommendList);
